a243e79ca8f165c13774761c80bbf2849ba7c2a0
YeahitsDivyansh <divyansh972003@gmail.com>
2024-11-14 00:04:26 +0530

    node-mailer setup

---
+++ src/helpers/mailer.ts
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import nodemailer from 'nodemailer';
+
+export const sendEmail = async({email, emailType, userId}
+    :any) =>{
+    try {
+        const transporter = nodemailer.createTransport({
+            host: "smtp.ethereal.email",
+            port: 587,
+            secure: false, // true for port 465, false for other ports
+            auth: {
+              user: "maddison53@ethereal.email",
+              pass: "jn7jnAPss4f63QBp6D",
+            },
+          });
+
+        const mailOptions = {
+            from: 'divyansh@divyansh.ai',
+            to: email,
+            subject: emailType === 'VERIFY' ? "Verify your email"
+            : "Reset your password",
+            html: "<b>Hello world?</b>",
+          }
+
+        const mailResponse = await transporter.sendMail(mailOptions);
+        return mailResponse;
+
+    } catch (error:any) {
+        throw new Error(error.message);
+    }
+}
